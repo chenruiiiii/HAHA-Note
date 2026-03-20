7f4e9ba8c529d95f3040178c6b7cdf6e5d58f0c1
chenrui <2659588544@qq.com>
2026-03-20 21:34:25 +0800

    fix: 修改sentry配置 - 添加webVital数据收集

---
+++ src/components/common/HAAILoading/index.tsx
@@ -0,0 +1,13 @@
+import { Spin } from 'antd';
+import React from 'react';
+import './style.scss';
+
+function HAAILoading() {
+  return (
+    <div className="ha-ai-loading">
+      <Spin></Spin>
+    </div>
+  );
+}
+
+export default HAAILoading;
