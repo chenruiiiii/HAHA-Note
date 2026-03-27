8403b30852c9bd9fb8efda4191956863877082ea
chenrui <2659588544@qq.com>
2026-03-27 21:57:44 +0800

    feat: 完成逛逛首页样式

---
+++ src/components/layout/stroll/components/RightRecommendItem/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './style.module.scss';
+import HAAvatar from '@/components/ui/HAAvatar';
+import { RightRecommendItemType } from '../../types/recommend';
+
+const RightRecommendItem = ({
+  id,
+  user: { avatar, username },
+  recommend_title,
+}: RightRecommendItemType) => {
+  return (
+    <div className={styles['recommend-item']}>
+      <div className={styles['info']}>
+        <HAAvatar size="middle" url={avatar} />
+        <div className={styles['right']}>
+          <div className={styles['username']}>{username} </div>
+          <div className={[styles['signature'], 'ellipse-one-line'].join(' ')}>
+            {recommend_title}
+          </div>
+        </div>
+      </div>
+      <div className="recommend-title">{recommend_title}</div>
+    </div>
+  );
+};
+
+export default RightRecommendItem;
